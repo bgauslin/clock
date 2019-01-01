97d21a37b6f53d1d9ea2d6957222b594ae5eba11
Ben Gauslin <ben@gauslin.com>
2019-01-01 12:54:21 -0500

    refactors JS entry file

--- source/js/clock.js
+++ source/js/clock.js
@@ -2,12 +2,6 @@ import { Clock } from './modules/customClock';
 import { Theme } from './modules/theme';
 import { Utilities } from './modules/utilities';
 
-/** @const {number} */
-const CLOCKS_COUNT = 9;
-
-/** @const {Element} */
-const clocksEl = document.querySelector('.clocks__frame');
-
 /** @instance */
 const theme = new Theme();
 
@@ -22,11 +16,21 @@ const utilities = new Utilities({
 // Register custom elements.
 customElements.define('n-clock', Clock);
 
-/** @description Waits until DOM is ready before initializing app. */
-document.addEventListener('DOMContentLoaded', () => {
-  for (let i = 1; i <= CLOCKS_COUNT; i++) {
+/**
+ * @description Renders custom elements for the app.
+ * @param {!Element} selector Parent element's selector for rendered clocks.
+ * @param {?number=} n Number of clocks to render.
+ */
+const renderClocks = (selector, n=9) => {
+  const clocksEl = document.querySelector(selector);
+  for (let i = 1; i <= n; i++) {
     clocksEl.innerHTML += '<n-clock></n-clock>';
   }
+}
+
+/** @description Initializes app once the DOM is ready. */
+document.addEventListener('DOMContentLoaded', () => {
+  renderClocks('.clocks__frame');
   theme.init();
   utilities.init();
 }, { once: true } );
