06909d4af548fed15fe57282b4d2ec02749c95a0
Ben Gauslin <ben@gauslin.com>
2019-01-30 07:31:08 -0500

    ontouchstart attribute removed since it's irrelevant

--- source/html/index.php
+++ source/html/index.php
@@ -30,7 +30,7 @@ if ($_SERVER['SERVER_NAME'] == $prod_server) {
     <link rel="stylesheet" href="<?php echo $css_path ?>">
   </head>
 
-  <body ontouchstart no-touch no-js>
+  <body no-touch no-js>
     <div class="clocks">
       <div class="clocks__frame"></div>
     </div>
