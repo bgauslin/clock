0138b7fe60baa9ff292af4ea3f901d80379fcd66
Ben Gauslin <ben@gauslin.com>
2019-07-08 21:02:26 -0400

    uglifyjs-webpack-plugin removed

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,9 @@
 const common = require('./webpack.common.js');
 const merge = require('webpack-merge');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: 'clock.[contenthash].js',
-  },
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
   },
 });
