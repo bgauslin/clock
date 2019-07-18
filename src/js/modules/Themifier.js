cebc80e9440e994546a85baf78a39e8eb69eb5a8
Ben Gauslin <ben@gauslin.com>
2019-07-18 18:52:04 -0400

    dramatically simplifies Themifier custom element

--- src/js/modules/Themifier.js
+++ src/js/modules/Themifier.js
@@ -12,56 +12,29 @@ class Themifier extends HTMLElement {
   constructor() {
     super();
 
-    /** @private {?string} */
-    this.initialTheme_ = null;
+    // Set theme to stored value if it exists; default otherwise.
+    this.setAttribute(THEME_ATTR,
+      localStorage.getItem(THEME_ATTR) || Theme.DEFAULT);
+
+    // Toggle theme when clicked.
+    this.addEventListener('click', () => {
+      const newTheme = (
+        this.getAttribute(THEME_ATTR) === Theme.DEFAULT
+        ? Theme.ALT
+        : Theme.DEFAULT
+      );
+      this.setAttribute(THEME_ATTR, newTheme);
+    });
   }
 
   static get observedAttributes() {
     return [THEME_ATTR];
   }
-  
-  /** @callback */
-  attributeChangedCallback(name) {
-    if (name === THEME_ATTR) {
-      this.updateTheme_();
-    }
-  }
 
   /** @callback */
-  connectedCallback() {
-    this.setInitialTheme_();
-    this.handleEvents_();
-  }
-
-  /**
-   * Sets initial color when custom element is first connected.
-   * @private
-   */
-  setInitialTheme_() {
-    this.initialTheme_ = localStorage.getItem('theme') || Theme.DEFAULT;
-    this.setAttribute(THEME_ATTR, this.initialTheme_);
-  }
-
-  /**
-   * Sets 'theme' attribute and stores its value for later visits.
-   * @private
-   */
-  updateTheme_() {
-    const theme = this.getAttribute(THEME_ATTR);
-    document.body.setAttribute(THEME_ATTR, theme);
-    localStorage.setItem('theme', theme);
-  }
-
-  /**
-   * Adds a DOM element that toggles the theme when clicked.
-   * @private
-   */
-  handleEvents_() {
-    this.addEventListener('click', (e) => {
-      const theme = this.getAttribute(THEME_ATTR);
-      const newTheme = (theme === Theme.DEFAULT) ? Theme.ALT : Theme.DEFAULT;
-      this.setAttribute(THEME_ATTR, newTheme);
-    });
+  attributeChangedCallback(name, oldValue, newValue) {
+    document.body.setAttribute(THEME_ATTR, newValue);
+    localStorage.setItem(THEME_ATTR, newValue);
   }
 }
 
