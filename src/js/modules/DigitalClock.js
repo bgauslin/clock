e4ff09c6c9c5b6633a985ad609fa70cb34efabe8
Ben Gauslin <github@gauslin.com>
2019-07-22 21:06:04 -0400

    setTime_() called immediately, then every second in DigitalClock.js

--- src/js/modules/DigitalClock.js
+++ src/js/modules/DigitalClock.js
@@ -5,21 +5,26 @@ class DigitalClock extends HTMLElement {
 
   /** @callback */
   connectedCallback() {
-    setInterval(() => {
-      const now = new Date();
-      this.textContent = now.toLocaleString('en-US', {
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        hour12: true,
-      });
-    }, 1000);
+    this.setTime_();
+    setInterval(() => this.setTime_(), 1000);
   }
   
   /** @callback */
   disconnectedCallback() {
     clearInterval();
   }
+
+  /** @private */
+  setTime_() {
+    const now = new Date();
+    this.textContent = now.toLocaleString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true,
+    });
+  }
+
 }
 
 export { DigitalClock };
