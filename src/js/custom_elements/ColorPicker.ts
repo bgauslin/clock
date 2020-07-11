cd187fe48ed3cfdc976da197f1d786ba4fe2f632
Ben Gauslin <bgauslin@gmail.com>
2020-07-11 09:31:36 -0400

    ColorPalette via single source in JS

--- src/js/custom_elements/ColorPicker.ts
+++ src/js/custom_elements/ColorPicker.ts
@@ -4,27 +4,27 @@ const COLOR_ATTR: string  = 'color';
 const ICON_PATH: string = 'M9.577 0.234 C4.917 1.144 1.162 4.889 0.244 9.534 -1.491 18.3 6.417 24.834 12.375 23.911 14.306 23.611 15.253 21.352 14.367 19.613 13.284 17.484 14.831 15 17.222 15 L20.958 15 C22.636 15 23.995 13.613 24 11.939 23.976 4.552 17.255 -1.261 9.577 0.234 Z M4.5 15 C3.67 15 3 14.33 3 13.5 3 12.67 3.67 12 4.5 12 5.33 12 6 12.67 6 13.5 6 14.33 5.33 15 4.5 15 Z M6 9 C5.17 9 4.5 8.33 4.5 7.5 4.5 6.67 5.17 6 6 6 6.83 6 7.5 6.67 7.5 7.5 7.5 8.33 6.83 9 6 9 Z M12 6 C11.17 6 10.5 5.33 10.5 4.5 10.5 3.67 11.17 3 12 3 12.83 3 13.5 3.67 13.5 4.5 13.5 5.33 12.83 6 12 6 Z M18 9 C17.17 9 16.5 8.33 16.5 7.5 16.5 6.67 17.17 6 18 6 18.83 6 19.5 6.67 19.5 7.5 19.5 8.33 18.83 9 18 9 Z';
 const OPEN_ATTR: string = 'open';
 
-const Colors: string[] = [
-  'white',
-  'darkred',
-  'crimson',
-  'tomato',
-  'orange',
-  'gold',
-  'khaki',
-  'yellowgreen',
-  'seagreen',
-  'teal',
-  'lightskyblue',
-  'steelblue',
-  'royalblue',
-  'midnightblue',
-  'indigo',
-  'blueviolet',
-  'violet',
-  'hotpink',
-  'silver',
-  'black',
+const ColorPalette: string[][] = [
+  ['white', 'black'],
+  ['darkred', 'white'],
+  ['crimson', 'white'],
+  ['tomato', 'black'],
+  ['orange', 'black'],
+  ['gold', 'black'],
+  ['khaki', 'black'],
+  ['yellowgreen', 'black'],
+  ['seagreen', 'white'],
+  ['teal', 'white'],
+  ['lightskyblue', 'black'],
+  ['steelblue', 'white'],
+  ['royalblue', 'white'],
+  ['midnightblue', 'white'],
+  ['indigo', 'white'],
+  ['blueviolet', 'white'],
+  ['violet', 'black'],
+  ['hotpink', 'black'],
+  ['silver', 'black'],
+  ['black', 'white'],
 ];
 
 /**
@@ -54,6 +54,7 @@ class ColorPicker extends HTMLElement {
 
   connectedCallback(): void {
     this.setup_();
+    this.createStyles_();
   }
 
   disconnectedCallback(): void {
@@ -146,16 +147,17 @@ class ColorPicker extends HTMLElement {
    * Creates elements and attaches them to the DOM.
    */
   private setup_(): void {
-    this.setAttribute(COLOR_ATTR, localStorage.getItem(COLOR_ATTR) || Colors[0]);
+    this.setAttribute(COLOR_ATTR, localStorage.getItem(COLOR_ATTR) || ColorPalette[0][0]);
 
     let listItems = '';
-    Colors.forEach((color) => {
-      const checked = (color === this.getAttribute(COLOR_ATTR)) ? 'checked' : '';
+    ColorPalette.forEach((color) => {
+      const [colorName] = color;
+      const checked = (colorName === this.getAttribute(COLOR_ATTR)) ? 'checked' : '';
       listItems += `\
         <li class="${this.className}__item">\
-          <label class="${this.className}__label" for="${color}" tabindex="0">\
-            <input class="${this.className}__option" type="radio" name="color" value="${color}" ${checked}>\
-            <span class="${this.className}__swatch" option="${color}">${color}</span>
+          <label class="${this.className}__label" for="${colorName}" tabindex="0">\
+            <input class="${this.className}__option" type="radio" name="color" value="${colorName}" ${checked}>\
+            <span class="${this.className}__swatch" option="${colorName}" color="${colorName}">${colorName}</span>
           </label>\
         </li>\
       `;
@@ -186,6 +188,25 @@ class ColorPicker extends HTMLElement {
 
     this.toggleButton_ = this.querySelector(`.${this.className}__toggle`);
     this.menu_ = this.querySelector(`.${this.className}__menu`);
+  }
+
+  /**
+   * Injects an inline <style> element with custom properties scoped to an
+   * attribute selector for each color option.
+   */
+  private createStyles_(): void {
+    const style = document.createElement('style');
+    ColorPalette.forEach((color) => {
+      const [colorName, constrastColor] = color;
+      const theme = `\
+        [color=${colorName}] {\
+          --face: ${colorName};\
+          --hands: ${constrastColor};\
+        }\
+      `;
+      style.innerHTML += theme.replace(/\s\s/g, '');
+    });
+    document.body.appendChild(style);
   }
 }
 
