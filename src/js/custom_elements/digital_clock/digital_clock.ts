a54a02c22150c055e1bf10e53f28ac8aa4c4009b
Ben Gauslin <bgauslin@gmail.com>
2021-02-15 16:26:15 -0500

    removes/replaces shifty-header; moves digital-clock inside the heading

--- src/js/custom_elements/digital_clock/digital_clock.ts
+++ src/js/custom_elements/digital_clock/digital_clock.ts
@@ -1,21 +1,15 @@
-const INTERVAL_MS: number = 1000;
-const TARGET_ATTR: string = 'target';
-
 /**
  * Custom element that renders a digital clock based on system time.
  */
 export class DigitalClock extends HTMLElement {
-  private targetEl: HTMLElement;
   private interval: any;
 
   constructor() {
     super();
-    this.interval = setInterval(() => this.setTime(), INTERVAL_MS);
+    this.interval = setInterval(() => this.setTime(), 1000);
   }
 
   connectedCallback() {
-    this.targetEl = this.querySelector(this.getAttribute(TARGET_ATTR));
-    this.removeAttribute(TARGET_ATTR);
     this.setTime();
   }
 
@@ -25,7 +19,7 @@ export class DigitalClock extends HTMLElement {
 
   private setTime() {
     const now = new Date();
-    this.targetEl.textContent = now.toLocaleString('en-US', {
+    this.textContent = now.toLocaleString('en-US', {
       hour: 'numeric',
       minute: 'numeric',
       second: 'numeric',
