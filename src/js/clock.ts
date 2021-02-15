fbf6b9dd66d3c13bfe13bd834ff69cf15a5857ae
Ben Gauslin <bgauslin@gmail.com>
2021-02-15 16:26:50 -0500

    minor updates to JS entry file

--- src/js/clock.ts
+++ src/js/clock.ts
@@ -3,27 +3,22 @@ require('dotenv').config();
 import './custom_elements/analog_clock/analog_clock';
 import './custom_elements/color_picker/color_picker';
 import './custom_elements/digital_clock/digital_clock';
-import './custom_elements/shifty_header/shifty_header';
 import './custom_elements/tools/tools';
-
-// Import styles for injecting into DOM.
 import '../stylus/index.styl';
 
-// Render analog clocks into <main> with an IIFE and add a class for styling.
+// Empty out <main>, add a classname for CSS, and render 9 analog clocks.
 (() => {
-  const count = 9;
   const main = document.querySelector('main');
+  main.classList.add('clocks');
 
   while (main.firstChild) {
     main.firstChild.remove();
   }
 
-  for (let i = 1; i <= count; i++) {
+  for (let i = 1; i <= 9; i++) {
     const clock = document.createElement('analog-clock');
     main.appendChild(clock);
   }
-
-  main.classList.add('clocks');
 })();
 
 // Register the Service Worker.
