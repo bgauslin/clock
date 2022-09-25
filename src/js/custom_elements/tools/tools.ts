bdbaf6bda47a5b5ff076e619747dd114394c5996
Ben Gauslin <github@gauslin.com>
2022-09-25 06:17:48 -0500

    Removes ga and dead viewportHeight calls

--- src/js/custom_elements/tools/tools.ts
+++ src/js/custom_elements/tools/tools.ts
@@ -9,33 +9,13 @@ export class Tools extends HTMLElement {
   constructor() {
     super();
     this.hasSetup = false;
-    window.addEventListener('resize', this.viewportHeight);
   }
 
   connectedCallback() {
     if (!this.hasSetup) {
       document.body.removeAttribute('no-js');
       this.touchEnabled();
-      this.googleAnalytics();
       this.hasSetup = true;
-    }
-  }
-
-  disconnectedCallback() {
-    window.removeEventListener('resize', this.viewportHeight);
-  }
-
-  /**
-   * Initializes Google Analytics tracking.
-   */
-  private googleAnalytics() {
-    if (process.env.NODE_ENV === 'production') {
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*(new Date() as any);a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      (window as any).ga('create', process.env.GA_ID, 'auto');
-      (window as any).ga('send', 'pageview');
     }
   }
 
